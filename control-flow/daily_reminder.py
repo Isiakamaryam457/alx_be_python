da76784c5089ef846d5f2a9b67585ea57d2a2f60
Isiaka Maryam <isiakamaryam457@gmail.com>
2025-11-16 12:03:40 +0100

    match case

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,19 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").strip().lower()
+time_bound = input("Is it time-bound? (yes/no): ").strip().lower()
+
+match priority:
+    case "high":
+       message = f"'{task}' is a high priority task"
+    case "medium":
+       message = f"'{task}' is a medium priority task"
+    case "low":
+       message = f"'{task}' is a low priority task"
+    case _:
+        message = f"'{task}' is a task with unspecified priority"
+
+if time_bound == "yes":
+    print(f"Reminder: {message} that requires immediate attention today!")
+else:
+    print(f"Note: {message} Consider completing it when you have free time.")
+       
